24d0625991896b065e2e001488428de08b9dcd1f
Armin Balalaie <armin.balalaie@gmail.com>
2019-05-21 21:06:05 -0700

    adds initial helper and test

--- failify/src/test/java/io/failify/examples/spark/FailifyHelper.java
+++ failify/src/test/java/io/failify/examples/spark/FailifyHelper.java
@@ -3,6 +3,7 @@ package io.failify.examples.spark;
 import io.failify.FailifyRunner;
 import io.failify.dsl.entities.Deployment;
 import io.failify.dsl.entities.PathAttr;
+import io.failify.dsl.entities.PortType;
 import io.failify.exceptions.RuntimeEngineException;
 import io.failify.execution.ULimit;
 import org.slf4j.Logger;
@@ -14,10 +15,11 @@ import java.util.StringJoiner;
 public class FailifyHelper {
     public static final Logger logger = LoggerFactory.getLogger(FailifyHelper.class);
 
-    public static Deployment getDeployment(int numOfSlaves) {
+    public static Deployment getDeployment(int numOfMasters, int numOfSlaves) {
         String version = "2.4.3"; // this can be dynamically generated from maven metadata
         String dir = "spark-" + version + "-bin-custom-spark";
         Deployment.Builder builder = Deployment.builder("example-spark")
+            .withService("zk").dockerImageName("zookeeper").disableClockDrift().and().withNode("zk1", "zk").and()
             .withService("spark-master")
                 .applicationPath("../spark-2.4.3-build/" + dir + ".tar.gz", "/spark", PathAttr.COMPRESSED)
                 .workDir("/spark/" + dir).startCommand("sbin/start-master.sh")
@@ -25,11 +27,40 @@ public class FailifyHelper {
                 .logDirectory("/spark/" + dir + "/logs").and()
             .withService("spark-slave")
                 .applicationPath("../spark-2.4.3-build/" + dir + ".tar.gz", "/spark", PathAttr.COMPRESSED)
-                .workDir("/spark/" + dir).startCommand("sbin/start-slave.sh http://master:8080")
+                .workDir("/spark/" + dir).startCommand("sbin/start-slave.sh -c 1 -m 1G " + getMasterString(numOfMasters))
                 .dockerImageName("failify/spark:1.0").dockerFileAddress("docker/Dockerfile", true)
-                .logDirectory("/spark/" + dir + "/logs").and()
-            .withNode("master", "spark-master").and();
-        for (int i=1; i<=numOfSlaves; i++) builder.withNode("slave" + i, "spark-slave").and();
+                .logDirectory("/spark/" + dir + "/logs").and();
+        for (int i=1; i<=numOfMasters; i++) builder.withNode("master" + i, "spark-master").offOnStartup()
+                .tcpPort(7077).environmentVariable("SPARK_DAEMON_JAVA_OPTS",
+                        "-Dspark.deploy.recoveryMode=ZOOKEEPER -Dspark.deploy.zookeeper.url=zk1:2181 -Dzookeeper.sasl.client=false").and();
+        for (int i=1; i<=numOfSlaves; i++) builder.withNode("slave" + i, "spark-slave").offOnStartup().and();
         return builder.build();
+    }
+
+    private static String getMasterString(int numOfMasters) {
+        StringJoiner joiner = new StringJoiner(",");
+        for (int i=1; i<=numOfMasters; i++) joiner.add("master" + i + ":7077");
+        return "spark://" + joiner.toString();
+    }
+
+    public static String getClientMasterString(FailifyRunner runner, int numOfMasters) {
+        StringJoiner joiner = new StringJoiner(",");
+        for (int i=1; i<=numOfMasters; i++) joiner.add(runner.runtime().ip("master" + i) + ":" +
+                runner.runtime().portMapping("master" + i, 7077, PortType.TCP));
+        return "spark://" + joiner.toString();
+    }
+
+    public static void startNodesInOrder(FailifyRunner runner) throws InterruptedException, RuntimeEngineException {
+        Thread.sleep(10000);
+        for (String node: runner.runtime().nodeNames()) {
+            if (node.startsWith("master"))
+                runner.runtime().startNode(node);
+        }
+        Thread.sleep(10000);
+        for (String node: runner.runtime().nodeNames()) {
+            if (node.startsWith("slave"))
+            runner.runtime().startNode(node);
+        }
+        Thread.sleep(10000);
     }
 }
--- failify/src/test/java/io/failify/examples/spark/SampleTest.java
+++ failify/src/test/java/io/failify/examples/spark/SampleTest.java
@@ -2,12 +2,14 @@ package io.failify.examples.spark;
 
 import io.failify.FailifyRunner;
 import io.failify.exceptions.RuntimeEngineException;
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import static org.junit.Assert.*;
 
 import java.sql.*;
 import java.util.concurrent.TimeoutException;
@@ -16,11 +18,15 @@ public class SampleTest {
     private static final Logger logger = LoggerFactory.getLogger(SampleTest.class);
 
     protected static FailifyRunner runner;
-    protected static final int NUM_OF_NODES = 3;
+    protected static final int NUM_OF_SLAVES = 3;
+    protected static final int NUM_OF_MASTERS = 3;
 
     @BeforeClass
-    public static void before() throws RuntimeEngineException, TimeoutException {
-        runner = FailifyHelper.getDeployment(NUM_OF_NODES).start();
+    public static void before() throws RuntimeEngineException, InterruptedException {
+        runner = FailifyHelper.getDeployment(NUM_OF_MASTERS, NUM_OF_SLAVES).start();
+        FailifyHelper.startNodesInOrder(runner);
+
+
         logger.info("The cluster is UP!");
     }
 
@@ -34,6 +40,17 @@ public class SampleTest {
 
     @Test
     public void sampleTest() throws RuntimeEngineException, SQLException, ClassNotFoundException, TimeoutException {
+        String logFile = "README.md"; // Should be some file on your system
+        SparkSession spark = SparkSession.builder().master(FailifyHelper.getClientMasterString(runner, NUM_OF_MASTERS))
+                .appName("Simple Application").getOrCreate();
 
+        Dataset<String> logData = spark.read().textFile(logFile).cache();
+
+        long numAs = logData.filter((FilterFunction<String>) s -> s.contains("a")).count();
+        long numBs = logData.filter((FilterFunction<String>) s -> s.contains("b")).count();
+
+        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+        spark.stop();
     }
 }
